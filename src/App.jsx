fdaeaa9c81bdb179d16e7463abfef4a70d8b13c5
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-05-16 20:48:23 +0100

    Search Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BsCalendar2HeartFill } from "react-icons/bs";
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import Search from "./components/Search.jsx"
+import { BsCalendar2HeartFill } from 'react-icons/bs';
+import { Col, Container, Row } from 'react-bootstrap';
+import Search from './components/Search.jsx';
 function App() {
-
   return (
     <>
-    <Container>
-      <Row>
-        <Col>
-        <h1 className='text-center fw-light mt-3'>
-            <BsCalendar2HeartFill className='me-2'/>
-         Appointments</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center fw-light mt-3 mb-3">
+              <BsCalendar2HeartFill className="me-3" />
+              Appointments
+            </h1>
+          </Col>
+        </Row>
 
-        </Col>
-      </Row>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <Search />
+          </Col>
+        </Row>
 
-      <Row className='justify-content-center'>
-        <Col md={4}>
-        <Search/>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col md={3}>
-        <h3>Hello Bootstrap!</h3>
-        </Col>
-        <Col md={3}>
-        <h3>Hello Bootstrap!</h3>
-        </Col>
-        <Col md={3}>
-        <h3>Hello Bootstrap!</h3>
-        </Col>
-        <Col md={3}>
-        <h3>Hello Bootstrap!</h3>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
