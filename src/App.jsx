9bf4309d01125cf098b2c7a78d0bfb98548a69ab
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-05-16 20:09:43 +0100

    Bootstrap Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsCalendar2HeartFill } from "react-icons/bs";
+import { Button, Col, Container, Row } from 'react-bootstrap';
 function App() {
 
   return (
     <>
+    <Container>
+      <Row>
+        <Col md={3}>
+        <h3>Hello Bootstrap!</h3>
+        </Col>
+        <Col md={3}>
+        <h3>Hello Bootstrap!</h3>
+        </Col>
+        <Col md={3}>
+        <h3>Hello Bootstrap!</h3>
+        </Col>
+      </Row>
+    </Container>
       <h1>
         <BsCalendar2HeartFill />
         Appointments</h1>
+        <Button>Hello!</Button>
     </>
   )
 }
