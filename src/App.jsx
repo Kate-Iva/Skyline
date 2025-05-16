628ad67b99a2aa8043e2b39f9a0599773bb51752
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-05-16 20:31:18 +0100

    Search Cpmonent Builded

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,31 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsCalendar2HeartFill } from "react-icons/bs";
 import { Button, Col, Container, Row } from 'react-bootstrap';
+import Search from "./components/Search.jsx"
 function App() {
 
   return (
     <>
     <Container>
       <Row>
+        <Col>
+        <h1 className='text-center fw-light mt-3'>
+            <BsCalendar2HeartFill className='me-2'/>
+         Appointments</h1>
+
+        </Col>
+      </Row>
+
+      <Row className='justify-content-center'>
+        <Col md={4}>
+        <Search/>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={3}>
+        <h3>Hello Bootstrap!</h3>
+        </Col>
         <Col md={3}>
         <h3>Hello Bootstrap!</h3>
         </Col>
@@ -19,10 +38,6 @@ function App() {
         </Col>
       </Row>
     </Container>
-      <h1>
-        <BsCalendar2HeartFill />
-        Appointments</h1>
-        <Button>Hello!</Button>
     </>
   )
 }
